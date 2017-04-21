f304d119dec5775dbecce808909cf972bdf43484
veera marni <narayana1043@gmail.com>
2017-04-21 14:13:52 -0400

    ubuntu backup

--- cmd_scripts/az_cmd.py
+++ cmd_scripts/az_cmd.py
@@ -1,10 +1,10 @@
+import time
 from cloudmesh_client.common.Shell import Shell
-from time import sleep
 from write2files import HostsWriter
 from IPy import IP
 import cmd
-import sys
-
+import os, sys
+import getpass
 
 class VMS(cmd.Cmd):
     
@@ -29,12 +29,11 @@ class VMS(cmd.Cmd):
     def __init__(self):
         
         cmd.Cmd.__init__(self)
-        self.prompt = '> '
+        self.prompt = '>> '
         self.n = 1
         self.floating_ip_list = []
         self.static_ip_list = []
         # self.setup()
-        
     
     def do_setCloud(self, cloud):
         
@@ -42,69 +41,111 @@ class VMS(cmd.Cmd):
         self.setup(cloud=self.cloud)
         
     def do_boot(self, n):
+    
+        self.floating_ip_list = []
+        self.static_ip_list = []
+        try:
         
-        print "Starting to boot", n ,"Virtual Machines"
+            for i in range(int(n)):
+    
+                print "Starting to boot Virtual Machine : ",i+1
+                Shell.cm("vm", "boot --secgroup=naveen-def")
+                fip_result = Shell.cm("vm", "ip assign")            # floating IP
+                floating_ip = fip_result.split(' ')[-2][:-6]
+                
+                try:
+                
+                    IP(floating_ip)
+                    # the below cmd is the "cm vm ip show" as ip is not getting updated automatically in the DB
+                    Shell.cm("vm", "ip assign")
+                    
+                    while True:
+                        
+                        sip_result = Shell.cm("vm", "ip show")          # static IP
+                        static_ip = sip_result.split("\n")[3].split(' ')[3]
+                        if IP(static_ip):
+                            
+                            break
+                    
+                except:
+                
+                    print "floating IP error encountered"
+                    print "Stopping to create further VMs"
+                    break
+    
+                self.floating_ip_list.append(floating_ip)
+                self.static_ip_list.append(static_ip)
         
-        for i in range(int(n)):
-                
-            Shell.cm("vm", "boot")
-            fip_result = Shell.cm("vm", "ip assign")            # floating IP
-            floating_ip = fip_result.split(' ')[-2][:-6]
-            try:
-                IP(floating_ip)
-                # the below cmd is the "cm vm ip show" as ip is not getting updated automatically in the DB
-                Shell.cm("vm", "ip assign")
-                sip_result = Shell.cm("vm", "ip show")          # static IP
-                static_ip = sip_result.split("\n")[3].split(' ')[3]
-                
-            except:
-                print "floating IP error encountered"
-                print "Stopping to create further VMs"
-                break
-
-            self.floating_ip_list.append(floating_ip)
-            self.static_ip_list.append(static_ip)
+        except ValueError:
+            
+            self.help_boot()
 
         if self.floating_ip_list == []:
+            
             print "No VMs created"
+        
         else:
+            
             print "Returning IPs of VMs created"
             print "Floating IPs list    :",self.floating_ip_list
             print "Static IPs list      :",self.static_ip_list
             print "wirting IPs to respective files ..."
         
-        HW = HostsWriter()
-        HW.writeFIPs(floatingIPs=self.floating_ip_list)
+            HW = HostsWriter()
+            HW.writeIPs(staticIPs=self.static_ip_list, floatingIPs=self.floating_ip_list)
+            
+            # starting ansible
+            if os.path.exists(os.environ['HOME']+'/.ssh/known_hosts'):
+                os.remove(os.environ['HOME']+'/.ssh/known_hosts')
+                
+            print "Running the ansible-playbook for zepplin"
+            
+            # taking password
+            password = getpass.getpass("Enter ansible valut password: ")
+            tempPassFile = open('.log.txt', 'w')
+            tempPassFile.write(password)
+            tempPassFile.close()
+            startTime = time.time()
+            deployment_logs = os.popen(
+                'ansible-playbook zeppelin/Zeppelin.yml -i zeppelin/hosts --vault-password-file .log.txt').read()
+            os.remove('.log.txt')
+            endTime = time.time()
+            
+            totalDeployTime = endTime - startTime
+            print "Time taken to depoly ",n," virtual machines for zeppelin is ",totalDeployTime
+            
+            # writing logs
+            tempDepLog = open('deployment_logs','w')
+            tempDepLog.write(deployment_logs)
+            tempDepLog.close()
+
+            # checking logs
+            deployment_logs_lines = deployment_logs.splitlines()
+
+            wordList = []
+            for line in deployment_logs_lines:
+                words = line.split(' ')
+                for word in words:
+                    wordList.append(word)
+
+            if "fatal" in wordList or '"Decryption' in wordList or "failed" in wordList or 'fatal:' in wordList:
+                print "Check deployment logs for errors during deployment"
+            else:
+                print "Deployment Successful"
 
     def do_delete(self, names):
         
         names = str(names).split(' ')
         for name in names:
+            
             delete_machine = "delete "+name
             print delete_machine
             result = Shell.cm("vm", delete_machine)
             print result
-    
-    def help_boot(self):
-        
-        print "syntax: boot [count]\n"
-        print "usage: "
-        print "       |  command   |  description                                        "
-        print "        ------------------------------------------------------------------"
-        print "          boot [n]     boots 3 vms one after the other"
 
     def do_quit(self, arg):
         
         sys.exit(1)
-
-    def help_quit(self):
-        
-        print "syntax: quit or q\n",
-        print "usage: "
-        print "       |  command   |  description                                        "
-        print "        ------------------------------------------------------------------"
-        print "          quit         terminates the application"
-        print "          q            terminates the application"
         
     def do_getFloatingIPs(self):
         
@@ -112,6 +153,31 @@ class VMS(cmd.Cmd):
 
     def do_getStaticIPs(self):
         print "Static IPs of all Machines", self.static_ip_list
+    
+    def default(self, line):
+        """Called on an input line when the command prefix is not recognized.
+           In that case we execute the line as Python code.
+        """
+        self.stdout.write('*** Unknown syntax: %s\n'%line)
+
+    # ---------Documentation-----------------
+
+    def help_boot(self):
+    
+        print "syntax: boot [count]\n"
+        print "usage: "
+        print "       |  command   |  description                                        "
+        print "        ------------------------------------------------------------------"
+        print "          boot [n]     boots 3 vms one after the other"
+
+    def help_quit(self):
+    
+        print "syntax: quit or q\n",
+        print "usage: "
+        print "       |  command   |  description                                        "
+        print "        ------------------------------------------------------------------"
+        print "          quit         terminates the application"
+        print "          q            terminates the application"
     
     def help_getFloatingIPs(self):
         
@@ -143,9 +209,11 @@ class VMS(cmd.Cmd):
     
         print "internal method"
 
-    # shortcuts
+    # ---------shortcuts----------------------
     do_q = do_quit
+    do_exit = do_quit
     help_q = help_quit
+    help_exit = help_quit
     
     
 if __name__ == "__main__":
--- cmd_scripts/write2files.py
+++ cmd_scripts/write2files.py
@@ -1,12 +1,66 @@
+import os
+
 class HostsWriter():
     
     def __init__(self):
-        self.file = open("hosts", "w")
-        self.file.write("[Zeppelin_hosts]\n")
-    
-    def writeFIPs(self, floatingIPs):
         
-        for fip in floatingIPs:
-            self.file.write(fip+" ansible_ssh_user=cc \n")
-        self.file.close()
+        # removing Ip config files
+        
+        # delete hosts
+        if os.path.isfile("./zeppelin/hosts") == True:
+            os.remove("./zeppelin/hosts")
+        
+        # delete sprak-defaults.conf
+        if os.path.isfile("./zeppelin/spark-defaults.conf") == True:
+            os.remove("./zeppelin/spark-defaults.conf")
+            
+        # delete spark-env.sh
+        if os.path.isfile("./zeppelin/spark-env.sh") == True:
+            os.remove("./zeppelin/spark-env.sh")
+
+        # delete spark-env.sh
+        if os.path.isfile("./zeppelin/slaves") == True:
+            os.remove("./zeppelin/slaves")
+    
+    def writeIPs(self, staticIPs, floatingIPs):
+        
+        # hosts recreation
+        file = open("./zeppelin/hosts", "w")
+        file.write("[All_nodes]\n")
+        
+        for sip,fip in zip(staticIPs,floatingIPs):
+            file.write(sip+" ansible_host="+fip+" ansible_ssh_user=cc \n")
+
+        file.write("\n\n")
+        file.write("[Master_node]\n")
+        file.write(staticIPs[-1]+" ansible_host="+floatingIPs[-1]+ " ansible_ssh_user=cc \n")
+
+        file.write("\n\n")
+        file.write("[Zeppelin_node]\n")
+        file.write(staticIPs[-1]+" ansible_host="+floatingIPs[-1]+ " ansible_ssh_user=cc \n")
+        
+        file.close()
+        
+        # sprak-defaults.conf recreation
+        file = open("./zeppelin/spark-defaults.conf", "w")
+        file.write("spark.master    spark://"+ staticIPs[-1] +":8081")
+        file.close()
+        
+        # spark-env.sh recreation
+        file = open("./zeppelin/spark-env.sh", "w")
+        file.write("# config file to be distributed across cluster \n")
+        file.write("SPARK_MASTER_HOST="+staticIPs[-1]+"\n")
+        file.write("SPARK_MASTER_PORT=8081\n")
+        file.write("SPARK_MASTER_WEBUI_PORT=8082\n")
+        file.close()
+        
+        # slaves recreation
+        file = open("./zeppelin/slaves", "w")
+                
+        if len(staticIPs) > 1:
+            for sip in staticIPs[:-1]:
+                file.write(sip+"\n")
+        elif len(staticIPs) == 1:
+            file.write(staticIPs[-1])
+        file.close()
             
